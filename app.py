ae9343749bbe5f5e2ca9b15acbea6a546fbe3ece
nyarkssss <wiafenyarkoa@gmail.com>
2024-09-04 14:09:47 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+
+
+# load pdf file 
+loader = DirectoryLoader("data/", glob="*pdf", loader_cls=PyPDFLoader)
+document = loader.load()
+
+
+# split document into chunks of text 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+text_chunks = text_splitter.split_documents(document)
+
+
+# create text embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': "cpu"})
+
+
+# create a vector store 
+vector_store = FAISS.from_documents(text_chunks, embeddings)
+
+
+# create LLM 
+llm = CTransformers(model="model\llama-2-7b-chat.ggmlv3.q4_0.bin", model_type="llama", config={'max_new_tokens':128, "temperature": 0.01})
+
+
+# create LLM memory 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+chain = ConversationalRetrievalChain.from_llm(llm=llm,chain_type="stuff",retriever=vector_store.as_retriever(search_kwargs={"k":2}), memory=memory)
+
+
+# create streamlit app
+st.title("HealthCare ChatBot")
+
+# "session_state_object: ", st.session_state
+
+
+def conversation_chat(query):
+    result=chain({'question': query,'chat_history': st.session_state['history']})
+    st.session_state['history'].append((query, result['answer']))
+    return result['answer']
+
+
+def initialize_session_state():
+    if "history" not in st.session_state:
+        st.session_state['history'] = []
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ['Hello! Ask me anything']
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ['Hey!']
+
+
+def display_chat_history():
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key="my_form", clear_on_submit=True):
+            user_input = st.text_input("Question: ", placeholder="Ask your question on Mental Health",key='input')
+            submit_button = st.form_submit_button(label="Send")
+
+            if submit_button and user_input:
+                output = conversation_chat(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['past'][i],is_user=True,key=str(i)+'_user',avatar_style="big-smile")
+                message(st.session_state['generated'][i],is_user=True,key=str(i),avatar_style="croodles")
+
+# initialize_session_state
+initialize_session_state()
+
+
+# display_chat_history
+display_chat_history()
+
